7b3a3e8eb59c29ab04b914ace220575e7d4b21c8
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-14 21:56:52 -0700

    Create + populate session reducer and store

--- frontend/spack.jsx
+++ frontend/spack.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionAPIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Splack</h1>, root);
 });
-
-window.SessionAPIUtil = SessionAPIUtil; 
