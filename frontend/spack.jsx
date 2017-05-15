c7f397f5d739f2fe07098ffd57025abe6d48952e
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-14 21:43:28 -0700

    Write and test AJAX signup/login/logout request

--- frontend/spack.jsx
+++ frontend/spack.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as SessionAPIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Splack</h1>, root);
 });
+
+window.SessionAPIUtil = SessionAPIUtil; 
