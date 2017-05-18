4b08c2e044057a35a370fd62a9a45c0dc567b6a4
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-18 10:37:24 -0700

    Fix messages persisting on each channel

--- frontend/components/home/main/chat/chat_list.jsx
+++ frontend/components/home/main/chat/chat_list.jsx
@@ -8,7 +8,15 @@ class ChatList extends React.Component {
 
   componentWillMount(){
     const channelId = this.props.match.params.channelId;
-    this.props.fetchMessages(channelId);
+
+  }
+
+  componentWillReceiveProps(newProps){
+    if (this.props.match.params.channelId != newProps.match.params.channelId) {
+      newProps.fetchMessages(newProps.match.params.channelId);
+      console.log("mounted");
+    }
+
   }
 
   render(){
@@ -37,5 +45,23 @@ class ChatList extends React.Component {
   }
 
 }
+//
+// <section className="all-messages-container">
+//   <ul className="chat-message-list">
+//          <li className="chat-message">
+      //   <div className="all-message-content">
+      //     <img src={message.user.avatar_url} />
+      //     <div className="message-content">
+      //       {message.user.username} <span id="message-time">{message.created_at}</span>
+      //       <br />
+      //       {message.content}
+      //     </div>
+      //   </div>
+      // </li>
+//   </ul>
+//   <footer id="new-message-footer">
+//     <NewMessageForm channel={this.props.channel} />
+//   </footer>
+// </section>
 
 export default withRouter(ChatList);
