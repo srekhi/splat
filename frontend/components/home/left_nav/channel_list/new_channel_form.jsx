6a82fedd9b5f119120997db25f538f812f2c6099
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-17 23:33:06 -0700

    Fix new channel modal closing always

--- frontend/components/home/left_nav/channel_list/new_channel_form.jsx
+++ frontend/components/home/left_nav/channel_list/new_channel_form.jsx
@@ -82,8 +82,12 @@ class NewChannelForm extends React.Component {
     }
     let channel = this.state;
     channel['user_ids'] = this.state.selectedUsers.map(user => user.id);
-    this.props.createChannel(channel).then(res => this.props.history.push(`/messages/${res.channel.id}`));
-    this.props.closeModal();
+    this.props.createChannel(channel).then( (res) => {
+      if (res.channel !== undefined) {
+        this.props.history.push(`/messages/${res.channel.id}`);
+        this.props.closeModal();
+      }
+    });
   }
 
   // renderErrors() {
