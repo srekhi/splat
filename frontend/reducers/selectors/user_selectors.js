f3c671a920755b0d39a68a7e160509877c8b10fb
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-19 11:11:23 -0700

    Remove Object.values

--- frontend/reducers/selectors/user_selectors.js
+++ frontend/reducers/selectors/user_selectors.js
@@ -1,3 +1,4 @@
 export const selectAllUsers = (state) => {
-  return Object.values(state.users);
+  // return Object.values(state.users);
+  return Object.keys(state.users).map(key => state.users[key]);
 };
