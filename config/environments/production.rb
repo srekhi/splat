12be08f8a1acb498d32a53cab8fe758f0e617cff
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-19 18:59:31 -0700

    Add configs for heroku live chat deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,11 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.web_socket_server_url = "wss://spack.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://spack.herokuapp.com', 'http://spack.herokuapp.com']
+
+
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
