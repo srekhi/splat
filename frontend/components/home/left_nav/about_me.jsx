bf6671fdc341f95fa536e33a45bd115e2461513d
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-22 20:07:54 -0700

    Begin styling about me section

---
+++ frontend/components/home/left_nav/about_me.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+const AboutMe = () =>(
+  <section id="about-container">
+    <section id="about-me-section">
+      <img id="about-logo"
+        src="http://res.cloudinary.com/dbbzpmyvc/image/upload/c_scale,w_28/v1494891230/slack_zawidf.svg" />
+
+      <section id="about-description-container">
+        <h4 id="about-header">About Splat</h4>
+        <p id="about-me-intro">
+          This application is an App Academy portfolio project inspired by
+          Slack.
+        </p>
+        <aside id="about-me-credits">Created by Sunny Rekhi</aside>
+    </section>
+
+    <div id="about-me-links">
+      <a className="about-me-link" href="mailto:rohit.rekhi@gmail.com">
+        <i className="fa fa-envelope"></i> E-mail
+      </a>
+      <a className="about-me-link" href="https://github.com/srekhi">
+        <i className="fa fa-github"></i> Github
+      </a>
+    </div>
+    </section>
+  </section>
+);
+
+export default AboutMe;
--- frontend/components/home/left_nav/left_nav.jsx
+++ frontend/components/home/left_nav/left_nav.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LeftNavHeaderContainer from './left_nav_header_container';
 import ChannelListContainer from './channel_list/channel_list_container';
+import AboutMe from './about_me.jsx';
 //eventually going to have LeftNav, Main, and Detail Component in here only.
 const LeftNav = () => {
   return(
     <main className="left-nav">
       <LeftNavHeaderContainer />
       <ChannelListContainer />
+      <AboutMe />
    </main>);
 };
 
