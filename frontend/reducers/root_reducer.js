9aa81865f213de87b92562319dc8437a1cc8c149
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-25 11:02:40 -0700

    Clear state after logout

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -5,12 +5,20 @@ import UserReducer from './user_reducer';
 import MessageReducer from './message_reducer';
 import GiphysReducer from './giphy_reducer';
 
-const RootReducer = combineReducers({
+
+const AppReducer = combineReducers({
   session: SessionReducer,
   channels: ChannelReducer,
   users: UserReducer,
   messages: MessageReducer,
   giphys: GiphysReducer
 });
+
+const RootReducer = (state, action) => {
+  if (action.type === "RECEIVE_CURRENT_USER" && action.currentUser === null){
+    state = undefined;
+  }
+  return AppReducer(state, action);
+};
 
 export default RootReducer;
