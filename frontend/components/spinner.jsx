0b32e21bdae3dd7f5e8b4e066b3c9aabdef86407
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-05-25 20:14:28 -0700

    Reposition spinner

--- frontend/components/spinner.jsx
+++ frontend/components/spinner.jsx
@@ -9,7 +9,7 @@ const Spinner = () => {
         src="http://res.cloudinary.com/dbbzpmyvc/image/upload/c_scale,h_100,w_100/v1495672697/slack_evh6h9.png"
       />
       <h1>Loading Splat...</h1>
-      
+      <br />
       <MDSpinner size={60} />
     </div>
   );
