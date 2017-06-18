7ecc9f329fa174a1c48ff728720813842783569d
Sunny Rekhi <rohit.rekhi@gmail.com>
2017-06-18 14:45:52 -0700

    Take intro message logic out of job

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,12 +8,13 @@ class Api::UsersController < ApplicationController
         Membership.create(user_id: @user.id, channel_id: channel.id)
       end
       # Notification.create()
-      IntroductionJob.perform_later(@user)
-      # intro_dm = Channel.create(name: "dm", private: true)
-      # sunny = User.find_by(username: "sunny")
-      # Membership.create(user_id: user.id, channel_id: intro_dm.id)
-      # Membership.create(user_id: sunny.id, channel_id: intro_dm.id)
-      # Message.create(user_id: sunny.id, channel_id: intro_dm.id, content: "Hi, I'm Sunny! Welcome to Splat!")
+      # IntroductionJob.perform_later(@user)
+      intro_dm = Channel.create(name: "dm", private: true)
+      sunny = User.find_by(username: "sunny")
+      Membership.create(user_id: @user.id, channel_id: intro_dm.id)
+      Membership.create(user_id: sunny.id, channel_id: intro_dm.id)
+      Message.create(user_id: sunny.id, channel_id: intro_dm.id, content: "Hi, I'm Sunny! Welcome to Splat!")
+
       render "api/users/show"
     else
       render json: @user.errors.full_messages, status: 422
